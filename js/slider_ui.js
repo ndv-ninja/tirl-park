a9f1f678321385c9f9bf76e404fc2905aaa34e2e
Kotovaa <sashjkeee@list.ru>
2015-07-31 12:44:56 +0300

    123

--- js/slider_ui.js
+++ js/slider_ui.js
@@ -2,11 +2,12 @@ $(function () {
     $("#range1").ionRangeSlider({
         hide_min_max: true,
         min: 1,
-        max: 3,
+        max: 22,
         from: 1,
-        to: 3,
+        to: 22,
         type: 'double',
-        step: 1
+        step: 1,
+        values_separator: ';'
     });
     $("#range2").ionRangeSlider({
         hide_min_max: true,
@@ -15,16 +16,20 @@ $(function () {
         from: 1,
         to: 3,
         type: 'double',
-        step: 1
+        step: 1,
+        values_separator: ';'
     });
-	$('.irs-slider.from').mouseenter(function(event) {
-		$(this).parent().find('.irs-from').addClass('hover');
-	}).mouseleave(function(event) {
-        $('.irs-slider.from').parent().find('.irs-from').removeClass('hover');
-    });
-	$('.irs-slider.to').mouseenter(function(event) {
-        $(this).parent().find('.irs-to').addClass('hover');
-    }).mouseleave(function(event) {
-        $('.irs-slider.to').parent().find('.irs-to').removeClass('hover');
-    });
+	// $('.irs-slider.from').mouseenter(function(event) {
+	// 	$(this).parent().find('.irs-from').addClass('hover');
+	// }).mouseleave(function(event) {
+ //        $('.irs-slider.from').parent().find('.irs-from').removeClass('hover');
+ //    });
+	// $('.irs-slider.to').mouseenter(function(event) {
+ //        $(this).parent().find('.irs-to').addClass('hover');
+ //    }).mouseleave(function(event) {
+ //        $('.irs-slider.to').parent().find('.irs-to').removeClass('hover');
+ //    });
+    if ($('.irs-slider').hasClass('state_hover')) {
+        $('.state_hover').addClass('hover');
+    }
 });
